1dbab14927925a25536b40bcf28723acc385f977
sajjadafzal <sajjadafzal@gmail.com>
2018-11-15 15:49:14 -0800

    Excel file reading implemented

--- renderer.js
+++ renderer.js
@@ -2,12 +2,13 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const remote = require('electron').remote;
+const Excel = require('xlsx');
 const { app, dialog } = remote;
 //var basepath = app.getAppPath();
 //document.getElementById('folderpath').innerHTML = basepath;
 
 document.getElementById('browseFile').onclick = () => {
-  console.log(dialog);
+  //console.log(dialog);
   dialog.showOpenDialog(
     {
       properties: ['openFile'],
@@ -25,3 +26,29 @@ document.getElementById('browseFile').onclick = () => {
     }
   );
 };
+
+var workbook = Excel.readFile('DataFile.xls');
+const datajs = Excel.utils.sheet_to_json(
+  workbook.Sheets[workbook.SheetNames[0]]
+);
+
+displayJSON(datajs);
+
+// var tbl = document.createElement('');
+function displayJSON(jsonData) {
+  var tabdiv = document.getElementById('DataSpace');
+  tabdiv.innerHTML = '';
+
+  var tbl = document.createElement('table');
+  Object.keys(datajs
+  //datajs.forEach(row => {
+    var rw = document.createElement('tr');
+    Object.values(row).forEach(ent => {
+      var cl = document.createElement('td');
+      cl.innerHTML = ent;
+      rw.append(cl);
+    });
+    tbl.append(rw);
+  });
+  tabdiv.append(tbl);
+}
